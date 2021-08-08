768b6266d3b0dca9f6ef2f0214418373dea5fb13
Thang Bui <thangbx@onemount.com>
2021-08-08 15:58:59 +0700

    Using uber fx for secured http client provider

--- httpclient.go
+++ httpclient.go
@@ -5,14 +5,27 @@ import (
 	secHttpClient "gitlab.id.vin/vincart/golib-security/web/client"
 	"gitlab.id.vin/vincart/golib/config"
 	"gitlab.id.vin/vincart/golib/web/client"
+	"go.uber.org/fx"
 )
 
-func UsingSecuredHttpClient(loader config.Loader) golib.ContextualHttpClientWrapper {
-	return func(client client.ContextualHttpClient) (client.ContextualHttpClient, error) {
-		securityProps, err := secHttpClient.NewSecurityProperties(loader)
-		if err != nil {
-			return nil, err
-		}
-		return secHttpClient.NewSecuredHttpClient(client, securityProps), nil
+type NewSecuredHttpClientIn struct {
+	fx.In
+	ConfigLoader config.Loader
+}
+
+type NewSecuredHttpClientOut struct {
+	fx.Out
+	Wrapper golib.ContextualHttpClientWrapper `group:"contextual_http_client_wrapper"`
+}
+
+func NewSecuredHttpClient(in NewSecuredHttpClientIn) NewSecuredHttpClientOut {
+	return NewSecuredHttpClientOut{
+		Wrapper: func(client client.ContextualHttpClient) (client.ContextualHttpClient, error) {
+			securityProps, err := secHttpClient.NewSecurityProperties(in.ConfigLoader)
+			if err != nil {
+				return nil, err
+			}
+			return secHttpClient.NewSecuredHttpClient(client, securityProps), nil
+		},
 	}
 }
